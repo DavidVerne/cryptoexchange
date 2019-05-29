98779d2bd61988f3d2dc908ea1daba9625f8934c
David Verne <davidverne@davids-air.home>
2019-05-29 08:07:35 +0100

    Updates

---
+++ precache-manifest.2f79c3a2157b78a2810b8dbe9a693f88.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a8726add3cb44a9fdb6f2c76b94a5f",
+    "url": "/cryptoexchange/index.html"
+  },
+  {
+    "revision": "55155436217c61a2e15e",
+    "url": "/cryptoexchange/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a6dbb5dfd074a1bd9ca3",
+    "url": "/cryptoexchange/static/css/main.10bc3b92.chunk.css"
+  },
+  {
+    "revision": "55155436217c61a2e15e",
+    "url": "/cryptoexchange/static/js/2.18d995f4.chunk.js"
+  },
+  {
+    "revision": "a6dbb5dfd074a1bd9ca3",
+    "url": "/cryptoexchange/static/js/main.d68ac2e9.chunk.js"
+  },
+  {
+    "revision": "c9366bc92fc204b234d8",
+    "url": "/cryptoexchange/static/js/runtime~main.63c86f44.js"
+  },
+  {
+    "revision": "a552edcdd0b2c25ee26fe0d1fb222761",
+    "url": "/cryptoexchange/static/media/bitcoin.a552edcd.png"
+  },
+  {
+    "revision": "bd866f788026421687a03fe5370807ce",
+    "url": "/cryptoexchange/static/media/ethereum.bd866f78.jpg"
+  },
+  {
+    "revision": "9ed19e76c8b23faee14c85fdf3da5d24",
+    "url": "/cryptoexchange/static/media/stellar.9ed19e76.png"
+  }
+]);
